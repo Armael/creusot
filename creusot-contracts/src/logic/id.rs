6eb1c8c6b72dc812ca781f5ec47e4e68c7013f7a
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-20 16:51:06 +0200

    Decouple the `Id` type from `pcell`

---
+++ creusot-contracts/src/logic/id.rs
@@ -0,0 +1,33 @@
+use crate::*;
+use ::std::marker::PhantomData;
+
+/// A unique id, usable in logic
+///
+/// The only properties of this type are:
+/// - there is an infinite number of ids
+/// - You can always generate a fresh id in logic code
+///
+/// # Usage
+///
+/// Ids are used in [`pcell`](crate::pcell) and [resource algebras](crate::resource).
+#[trusted]
+#[allow(dead_code)]
+pub struct Id(PhantomData<()>);
+
+impl Clone for Id {
+    #[pure]
+    #[ensures(result == *self)]
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+impl Copy for Id {}
+
+impl Id {
+    /// Generate a fresh id
+    #[logic]
+    #[open(self)]
+    pub fn fresh() -> Self {
+        crate::util::such_that(|_| true)
+    }
+}
